bbe2566d9094f2575940e90379dac5eb57985a92
James Spencer <jameskcs1254@gmail.com>
2021-09-21 14:24:00 +0100

    split tests into multiple suites

--- tests/factories.test.js
+++ tests/factories.test.js
@@ -1,6 +1,22 @@
 import shipFactory from "../src/factories";
 
-test('ship factory creates a ship of length 2 with hitBoard of 2', () => {
-  const ship = {hitboard:[0,0]};
-  expect(shipFactory(2)).toMatchObject(ship);
-})
+describe('ship factory tests', () => {
+
+  test('ship factory creates a ship of length 2 with hitBoard of 2', () => {
+    expect(shipFactory(2)).toMatchObject({hitBoard:[0,0]});
+  });
+
+  test('ship factory creates a ship of length 3 with hitBoard of 3', () => {
+    expect(shipFactory(3)).toMatchObject({hitBoard:[0,0,0]});
+  });
+});
+
+describe('ship object: hit method tests', () => {
+  const exampleShip = shipFactory(3);
+  
+  test('ship hit example 1', () => {
+    expect(exampleShip.hit(1)).toBe()
+
+  });
+});
+
