14a25847803b12cd0aa0768bf43f522591fb255f
James Spencer <jameskcs1254@gmail.com>
2021-09-21 14:35:21 +0100

    fixed formatting

--- src/factories.js
+++ src/factories.js
@@ -1,15 +1,15 @@
 const shipFactory = (length) => {
-  const hitBoard = new Array(length).fill(0);
+  const hitBoard = new Array(length).fill(0)
 
   const hit = (position) => {
-    hitBoard[position] = 1;
+    hitBoard[position] = 1
   }
 
   const isSunk = () => {
-    hitBoard.every(x => x === 1);
+    hitBoard.every(x => x === 1)
   }
 
-  return {hitBoard, isSunk, hit}
+  return { hitBoard, isSunk, hit }
 }
 
 export default shipFactory
