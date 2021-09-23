d92616b2eff90d682e1c512770db383bf13f7c29
James Spencer <jameskcs1254@gmail.com>
2021-09-23 12:05:43 +0100

    Add simple implementation of placeship function with helper function for coordinates

--- src/factories.js
+++ src/factories.js
@@ -12,10 +12,21 @@ const shipFactory = (length) => {
 
   return { hitBoard, isSunk, hit }
 }
+
 const gameBoardFactory = () => {
   const gameBoard = new Array(100).fill('')
 
-  return { gameBoard }
+  const coordinateTranslate = (x, y) => { 
+    const arrayIndex = ((y-1)*10) + (x-1)
+    return arrayIndex
+  }
+
+  const placeShip = (x, y) => {
+    gameBoard[coordinateTranslate(x,y)] = 0
+    return gameBoard
+  }
+
+  return { gameBoard, placeShip }
 }
 
 export { shipFactory, gameBoardFactory }
