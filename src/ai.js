cafca766e7cf96fd421ef09cea2de026995c5ea6
James Spencer <jameskcs1254@gmail.com>
2021-10-23 17:20:42 +0100

    add checkMove and refactor computerTurn

--- src/ai.js
+++ src/ai.js
@@ -1,16 +1,23 @@
 const ai = {
   moveList: [],
 
+  checkMove: (x,y) => {
+    return ai.moveList.every( i => (i.x !== x) && (i.y !== y))
+  },
 
+  computerTurn: () => {
+    const validMoveFound = false
 
-  computerTurn : () => {
-    const x = Math.round(Math.random()*10)
-    const y = Math.round(Math.random()*10)
+    while(!validMoveFound) {
+      const x = Math.round(Math.random()*10)
+      const y = Math.round(Math.random()*10)
 
-    return { x, y }
+      if(ai.checkMove(x,y)) {
+        ai.validMoveFound = true
+        return { x, y }
+      }
+    }
   }
-
-
 }
 
 export default ai
