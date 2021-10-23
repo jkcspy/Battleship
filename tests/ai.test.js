516b0db7867aa625618ea24fd39a63b6645f7756
James Spencer <jameskcs1254@gmail.com>
2021-10-23 16:14:02 +0100

    make range for random move to be within

--- tests/ai.test.js
+++ tests/ai.test.js
@@ -2,10 +2,15 @@ import ai from "../src/ai";
 
 describe('tests for AI random moves', () => {
   beforeEach(() => {
-    ai.clearMoveList();
+    ai.moveList = []
   })
 
   test('ai returns random coordiantes', () => {
-    expect(ai.computerTurn().x).toBeGreaterThanOrEqual()
+    const moveObject = ai.computerTurn()
+
+    expect(moveObject.x).toBeGreaterThanOrEqual(0)
+    expect(moveObject.x).toBeLessThanOrEqual(10)
+    expect(moveObject.y).toBeGreaterThanOrEqual(0)
+    expect(moveObject.y).toBeLessThanOrEqual(10)
   })
 })
