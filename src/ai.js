9812bc87097f8794481a23a8cfecef12c100c76e
James Spencer <jameskcs1254@gmail.com>
2021-10-23 17:00:35 +0100

    refactor computer turn to be less verbose

--- src/ai.js
+++ src/ai.js
@@ -1,13 +1,16 @@
 const ai = {
-   moveList : [],
-   computerTurn: () => {
-     const xFloat = Math.random()*10
-     const yFloat = Math.random()*10
-     return {
-      x: Math.round(xFloat),
-      y: Math.round(yFloat)
-    }
-   }
+  moveList: [],
+
+
+
+  computerTurn : () => {
+    const x = Math.round(Math.random()*10)
+    const y = Math.round(Math.random()*10)
+
+    return { x, y }
+  }
+
+
 }
 
 export default ai
