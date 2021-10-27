741a6b2378f73f8b2158856c9dede0c892d56846
James Spencer <jameskcs1254@gmail.com>
2021-10-27 14:31:35 +0100

    add function to translate array to coordinates

--- src/utils.js
+++ src/utils.js
@@ -3,4 +3,13 @@ const coordinateTranslate = (x, y) => {
   return arrayIndex
 }
 
-export default coordinateTranslate
+const arrayTranslate = (index) => {
+  const adjustedIndex = index+1
+  const indexString = adjustedIndex.toString()
+  const x = indexString.slice(-1) === '0' ? 10 : parseInt(indexString.slice(-1)) //no
+  const y = Math.ceil((adjustedIndex/10)-0.01) //yes
+
+  return { x, y }
+}
+
+export { coordinateTranslate, arrayTranslate }
