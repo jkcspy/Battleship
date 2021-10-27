0d7c60f7611be87bcdd687b01f6aca59bdeffa30
James Spencer <jameskcs1254@gmail.com>
2021-10-27 14:30:56 +0100

    add tests for arrayTranslate

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,4 @@
-import coordinateTranslate from "../src/utils";
+import {coordinateTranslate, arrayTranslate } from "../src/utils";
 
 describe('coordinate translate tests', () => {
   test('example 1', () => {
@@ -9,5 +9,17 @@ describe('coordinate translate tests', () => {
   })
   test('example 3', () => {
     expect(coordinateTranslate(5,1)).toBe(4)
+  })
+})
+
+describe('arrayTranslate tests', () => {
+  test('example 1', () => {
+    expect(arrayTranslate(0)).toEqual({x:1, y:1})
+  })
+  test('example 2', () => {
+    expect(arrayTranslate(99)).toEqual({x:10, y:10})
+  })
+  test('example 3', () => {
+    expect(arrayTranslate(9)).toEqual({x:10, y:1})
   })
 })
