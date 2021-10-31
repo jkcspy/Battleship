490c632d4bbf4774b035dc47e71a4cca5fa78429
James Spencer <jameskcs1254@gmail.com>
2021-10-31 10:32:10 +0000

    remove example ship placement

--- src/game.js
+++ src/game.js
@@ -7,13 +7,6 @@ const initGame = () => {
   const playerTwo = playerFactory('Player 2')
   const playerOneBoard = gameBoardFactory()
   const playerTwoBoard = gameBoardFactory()
- 
-  //remove later initial setup 
-  playerOneBoard.placeShip(2, 2, 2, 'down')
-  playerOneBoard.placeShip(5, 6, 3, 'down')
-  playerTwoBoard.placeShip(1, 1, 2, 'down')
-  playerTwoBoard.placeShip(5, 4, 3, 'down')
-
 
   return { playerOne, playerOneBoard, playerTwo, playerTwoBoard }
 }
