26d2cde9c4ab1edd35f24470a17a3974e1b299e1
James Spencer <jameskcs1254@gmail.com>
2021-11-01 14:04:10 +0000

    Complete viewButtonListener function
    I finished the event listener for button  which dispalys the ship at the user's
     choice of direction and coordinates by chaninging the class of the grid boxes.

--- src/dom-listeners.js
+++ src/dom-listeners.js
@@ -1,4 +1,4 @@
-import { arrayTranslate } from "./utils"
+import { arrayTranslate, coordinateTranslate, shipTypeCheck } from "./utils"
 import ai from "./ai"
 
 const attackListeners = (gameBoardTwoObject, playerOneObject, gameBoardOneObject, playerTwoObject) => {
@@ -25,12 +25,55 @@ const directionButtonListener = () => {
 
 }
 
-const viewButtonListener = () => {
+const buttonsObject = {
+  viewButton : document.querySelector('.view'),
+  xInput : document.querySelector('#x-input'),
+  yInput : document.querySelector('#y-input'),
+  directionButton : document.querySelector('.direction')
+}
 
+const viewButtonListener = (buttonsObject) => {
+  buttonsObject.viewButton.addEventListener('click', () => {
+    const x = parseInt(buttonsObject.xInput.value)
+    const y = parseInt(buttonsObject.yInput.value)
+    const direction = buttonsObject.directionButton.innerHTML.toLowerCase()
+    const shipIndex = []
+    const grid = document.querySelector('.placement-grid')
+    let gridBoxArray = []
+
+    if(direction === 'down'){
+      for(let i = 0; i < shipTypeCheck(); i++){
+        shipIndex.push(coordinateTranslate(x,y+i))
+      }
+    } 
+    if(direction === 'up'){
+      for(let i = 0; i < shipTypeCheck(); i++){
+        shipIndex.push(coordinateTranslate(x,y-i))
+      }
+    } 
+    if(direction === 'right'){
+      for(let i = 0; i < shipTypeCheck(); i++){
+        shipIndex.push(coordinateTranslate(x+i,y))
+      }
+    } 
+    if(direction === 'left'){
+      for(let i = 0; i < shipTypeCheck(); i++){
+        shipIndex.push(coordinateTranslate(x-i,y))
+      }
+    } 
+
+    shipIndex.forEach(item => {
+      gridBoxArray.push(grid.querySelector(`[data-index='${item}'`)) 
+    })
+
+    gridBoxArray.forEach( item => {
+      item.classList.add('ship')
+    })
+  })
 }
 
 const confirmButtonListener = () => {
-  
+
 }
 
-export { attackListeners } 
+export { attackListeners, directionButtonListener, viewButtonListener, confirmButtonListener, buttonsObject } 
