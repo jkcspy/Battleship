7a8a5807e8db8e6a5cdebfc4a0f7aae26fc95864
James Spencer <jameskcs1254@gmail.com>
2021-11-02 16:45:55 +0000

    call placePlayerShips in initGame

--- src/game.js
+++ src/game.js
@@ -1,12 +1,21 @@
 import { playerFactory, gameBoardFactory } from './factories'
 import { renderPlayerGrid, renderOpponentGrid } from './dom-render'
 import { attackListeners } from './dom-listeners'
+import { placePlayerShips } from './placement'
 
 const initGame = () => {
   const playerOne = playerFactory('Player 1')
   const playerTwo = playerFactory('Player 2')
   const playerOneBoard = gameBoardFactory()
   const playerTwoBoard = gameBoardFactory()
+
+  placePlayerShips(playerOneBoard)
+  // TODO: function for placing AI ships
+  playerTwoBoard.placeShip(1,1,5,'down')
+  playerTwoBoard.placeShip(2,1,4,'down')
+  playerTwoBoard.placeShip(3,1,3,'right')
+  playerTwoBoard.placeShip(2,9,3,'right')
+  playerTwoBoard.placeShip(5,5,2,'down')
 
   return { playerOne, playerOneBoard, playerTwo, playerTwoBoard }
 }
