7b787b5ce172e3871b3b567586a99e9887a6bd7a
James Spencer <jameskcs1254@gmail.com>
2021-11-02 15:28:59 +0000

    Add placeplayerShips function.
    I created a function to take the data from the ship placement setup and loop
    over it calling the gameboard method to place ships.

--- src/placement.js
+++ src/placement.js
@@ -1,3 +1,8 @@
 const shipPlacementData = [] // array to hold coordinates objects for ship placement
 
-export { shipPlacementData }
+const placePlayerShips = gameBoardObject => {
+  shipPlacementData.forEach(item => {
+    gameBoardObject.placeShip(item.x, item.y, item.length, item.direction)
+  })
+}
+export { placePlayerShips, shipPlacementData }
