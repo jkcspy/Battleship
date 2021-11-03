861b52462d1f23a1de4e2c2bbaa4015878cfd37b
James Spencer <jameskcs1254@gmail.com>
2021-11-03 18:38:22 +0000

    convert all listener function to one call

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { viewButtonListener, buttonsObject, directionButtonListener, muteButtonListener, confirmButtonListener, startGameListener } from './dom-listeners'
+import { attachAllEventListeners, buttonsObject } from './dom-listeners'
 import { initRenderGrid } from './dom-render'
 
 initRenderGrid()
-viewButtonListener(buttonsObject)
-directionButtonListener(buttonsObject)
-confirmButtonListener(buttonsObject)
-muteButtonListener()
-startGameListener()
+attachAllEventListeners(buttonsObject)
